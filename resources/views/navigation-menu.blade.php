69bedbd4c854595199500b4682156abee0920441
Elif Nur Halat <61434382+ElifNurHalat1@users.noreply.github.com>
2026-03-23 00:45:39 +0300

    Create Room model, migration and controller

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -79,11 +79,8 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-
+                          
+                     
                             <x-dropdown-link href="{{ route('profile.show') }}">
                                 {{ __('Profile') }}
                             </x-dropdown-link>
@@ -94,7 +91,7 @@
                                 </x-dropdown-link>
                             @endif
 
-                            <div class="border-t border-gray-200"></div>
+                          
 
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" x-data>
