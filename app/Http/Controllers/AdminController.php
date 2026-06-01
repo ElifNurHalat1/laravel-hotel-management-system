1be467544f4efe6abb10f100da3b5cc8684615c7
Elif Nur Halat <61434382+ElifNurHalat1@users.noreply.github.com>
2026-06-01 16:29:41 +0300

    Update AdminController and room view with today\'s additions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,8 @@ class AdminController extends Controller
             
             if($usertype == 'user')
             {
-                return view('home.index');
+                $room = Room::all();        
+                return view('home.index'   , compact('room'));
             }
             else if($usertype == 'admin')
             {
@@ -37,8 +38,8 @@ class AdminController extends Controller
 
     public function home()
     {
-        return view('home.index');
-
+        $room = Room::all();
+        return view('home.index', compact('room'));
     }
 
     public function create_room()
